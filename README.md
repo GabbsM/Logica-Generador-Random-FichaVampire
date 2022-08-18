6ac2b5eb15ca2e8228d2783e26949294bd71aeff
Gerard Montes <gmvelencoso@gmail.com>
2022-08-18 18:58:51 +0200

    generate spring boot project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Generador de fichas de personaje de Vampire
+
+Este microservicio (backend) se encargara de generar, guardar y servir fichas de personaje, controlando atributos y asignando puntos.
+
---
+++ src/test/java/com/gabbsm/vampire/VampireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabbsm.vampire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VampireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
